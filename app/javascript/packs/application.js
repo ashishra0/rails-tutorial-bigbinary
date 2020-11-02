41304fc8384759ccd72444fb19ebe130edeec78a
Ashish Rao <ashishrao2598@gmail.com>
2020-11-02 13:49:36 +0530

    Setup tailwindcss

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,6 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
@@ -14,3 +13,4 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+import "../stylesheets/application.scss"
